563981106824fe9d19dd09f90d86661505504866
wnukiewiczd <dawid.wnukiewicz@student.put.poznan.pl>
2025-06-15 20:59:10 +0200

    poprawa checkthreadsuupport

--- main.c
+++ main.c
@@ -34,25 +34,13 @@ void finalizuj()
 
 void check_thread_support(int provided)
 {
-    printf("THREAD SUPPORT: chcemy %d. Co otrzymamy?\n", provided);
-    switch (provided) {
-        case MPI_THREAD_SINGLE: 
-            printf("Brak wsparcia dla wątków, kończę\n");
-            /* Nie ma co, trzeba wychodzić */
-	    fprintf(stderr, "Brak wystarczającego wsparcia dla wątków - wychodzę!\n");
-	    MPI_Finalize();
+    printf("THREAD SUPPORT: chcemy %d.\n", provided);
+
+    if(provided == MPI_THREAD_MULTIPLE){
+        printf("Pełne wsparcie dla wątków\n");   
+    } else {
+        MPI_Finalize();
 	    exit(-1);
-	    break;
-        case MPI_THREAD_FUNNELED: 
-            printf("tylko te wątki, ktore wykonaly mpi_init_thread mogą wykonać wołania do biblioteki mpi\n");
-	    break;
-        case MPI_THREAD_SERIALIZED: 
-            /* Potrzebne zamki wokół wywołań biblioteki MPI */
-            printf("tylko jeden watek naraz może wykonać wołania do biblioteki MPI\n");
-	    break;
-        case MPI_THREAD_MULTIPLE: printf("Pełne wsparcie dla wątków\n"); /* tego chcemy. Wszystkie inne powodują problemy */
-	    break;
-        default: printf("Nikt nic nie wie\n");
     }
 }
 
